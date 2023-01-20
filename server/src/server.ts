ef61c9dbdc9fa8ec2bc8e37854f503dfd2e39b5b
Naiara Borges <naiarazk@gmail.com>
2023-01-19 23:40:39 -0300

    Estruturando banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,13 @@
 //Back-end API RESTful
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import { appRoutes } from './routes';
 
 const app = Fastify()
-const prisma = new PrismaClient()
+
 
 app.register(cors)
-
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany()
-  return habits
-})
+app.register(appRoutes)
 
 app.listen({
   port: 3333,
