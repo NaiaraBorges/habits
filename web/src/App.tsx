2691dab4a2dd41b7a0f6efaee8b6b907cfe05f5d
Naiara Borges <naiarazk@gmail.com>
2023-01-20 14:11:22 -0300

    Estruturando Home Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
-import './styles/global.css';
+import "./styles/global.css";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 
-function App() {
+export function App() {
   return (
-    <h1>Hello World</h1>
-  )
-}
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16 ">
+        <Header />
+        <SummaryTable />
 
-export default App
+      </div>
+    </div>
+  );
+}
